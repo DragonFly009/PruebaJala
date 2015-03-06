8747f535762c42bf36428a6220e3a49188ec395e
Samuel Vargas <varguitas0911@gmail.com>
2015-03-06 16:32:14 -0400

    upload practice

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*Analisando la edad de una persona*/
+
+var getEdad = function(){
+
+var name = prompt("Ingrese su nombre");
+var age = prompt("Ingrese su edad");
+
+    if (age <= 18) {
+
+        alert("Usted es tiene " + age + " años y es mayor de edad");
+
+        }else {
+
+        alert("Usted tiene " + age + " años y es menor de edad");
+    }
+};
---
+++ js/strings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by samuel on 18/04/2015.
+ */
+
+
+var cad = function(){
+    var numPalabras = prompt("Ingrese un Parrafo");
+    numPalabras.split();
+};
