2e13144b84e5b73d054f4b46c29ccfd7dbf80499
Samuel Vargas Arce <varguitas0911@gmail.com>
2015-04-18 02:34:39 -0400

    upload practice

--- js/strings.js
+++ js/strings.js
@@ -5,5 +5,12 @@
 
 var cad = function(){
     var numPalabras = prompt("Ingrese un Parrafo");
-    numPalabras.split();
+    var cont = numPalabras.split(" ");
+    //alert("el numero de palabras es " + cont.length);
+
+    for(var i = 0; i<cont.length; i++){
+        console.log( " * " + cont[i] + " * ");
+        document.write("<br /> * "  + cont[i] + " * ");
+    }
+
 };
